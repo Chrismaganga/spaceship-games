41007e6563a5ae9643e27f3fb73ae4e494964722
Chrismaganga <chrismaganga3@gmail.com>
2024-09-26 10:27:40 +0200

    app setup

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/features/crew/List';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text><Home/></Text>
       <StatusBar style="auto" />
     </View>
   );
